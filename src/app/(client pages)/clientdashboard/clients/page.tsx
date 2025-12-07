d2b9d5ba40b43cf16bcace5aeaf75165a3478d36
oo11boy <editkhone@gmail.com>
2025-12-07 19:41:41 +0330

    footer fix

--- src/app/(client pages)/clientdashboard/clients/page.tsx
+++ src/app/(client pages)/clientdashboard/clients/page.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Footer from '../components/Footer/Footer'
 
 export default function page() {
   return (
+   <div className="h-screen text-white overflow-auto max-w-md m-auto">
+  
+    
     <div>clientspage</div>
+
+    <Footer/>
+    </div>
   )
 }
