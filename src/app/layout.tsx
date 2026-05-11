96825a5468afbc9ecc451c0dc40f678bcc45d27a
oo11boy <editkhone@gmail.com>
2026-05-12 02:15:05 +0430

    fix size

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,10 +77,12 @@ export default function RootLayout({
   return (
     <html lang="fa" dir="rtl" suppressHydrationWarning>
       <head>
+        
         <link rel="manifest" href="/manifest.json" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="آنتایم" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes, viewport-fit=cover" />
         <Script
           id="global-json-ld"
           type="application/ld+json"
